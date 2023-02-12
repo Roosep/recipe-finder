0e699ff14deef2abb1bdda0c86aec6650c27eae7
Roosep <roopes16@hotmail.com>
2023-02-12 21:58:27 +0200

    Finished app

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, StyleSheet, Text, View, Image, FlatList, TextInput, Button} from 'react-native';
 
 export default function App() {
+  const [filter, setFilter] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  const getRecipes = () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=' + filter)
+    .then(response => response.json())
+    .then(data => setRecipes(data.meals))
+    .catch(error => {
+      Alert.alert("Error", error);
+    });
+  }
+
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        keyExtractor={(item, index) => item.idMeal}
+        renderItem={({item}) =>
+        <View>
+          <Text style={{fontSize: 17}}>{item.strMeal}</Text>
+          <Image source={{uri: item.strMealThumb}}
+          style={{width: 50, height: 50}}/>
+        </View>}
+        data={recipes}
+        ItemSeparatorComponent={listSeparator} 
+      />
+
+      <TextInput style={{fontSize: 18, width: 200}} 
+      placeholder="Filter"
+      value={filter}
+      onChangeText={text => setFilter(text)}/>
+      <Button title='Find' onPress={getRecipes} />
     </View>
   );
 }
